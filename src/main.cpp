afdeabab072583218f6c0c6ff677a87a50c7d92c
danielsehr <daniel.serr7028@gmail.com>
2026-04-26 12:36:11 +0200

    add: implented new .h files, build successfully

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,30 @@
 #include <Arduino.h>
+#include "config/SystemConfig.h"
+#include "core/SystemTypes.h"
 
-// put function declarations here:
-int myFunction(int, int);
+
+// Functions
+void initializePins() {
+  pinMode(Config::PIN_HEATER, OUTPUT);
+  pinMode(Config::PIN_STIRRER, OUTPUT);
+  pinMode(Config::PIN_LIGHT, OUTPUT);
+  pinMode(Config::PIN_STATUS_LED, OUTPUT);
+
+  pinMode(Config::PIN_RESET, INPUT_PULLUP);
+
+  pinMode(Config::PIN_TDS, INPUT);
+  pinMode(Config::PIN_TURBIDITY, INPUT);
+
+  pinMode(Config::PIN_TRIG, OUTPUT);
+  pinMode(Config::PIN_ECHO, INPUT);
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  initializePins();
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
