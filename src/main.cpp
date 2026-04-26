5ba0774b46ae3a04d1b1a368c8de966ce1991a07
danielsehr <daniel.serr7028@gmail.com>
2026-04-26 17:39:14 +0200

    add: sensor/control/log interval, initialisation and logic for caling the class methods  in void loop

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,26 @@
 #include "config/SystemConfig.h"
 #include "core/SystemTypes.h"
 #include "storage/SettingsManager.h"
+#include "control/ControlManager.h"
+#include "sensors/SensorManager.h"
 
+
+// Instantiate manager and settings
+SensorManager sensorManager;
 SettingsManager settingsManager;
+ControlManager controlManager;
+
 SystemSettings settings;
+
+
+// Define timers and intervals
+unsigned long lastSensorUpdate = 0;
+unsigned long lastControlUpdate = 0;
+unsigned long lastLogUpdate = 0;
+
+constexpr unsigned long SENSOR_INTERVAL_MS = 1000;
+constexpr unsigned long CONTROL_INTERVAL_MS = 1000;
+constexpr unsigned long LOG_INTERVAL_MS = 1000;
 
 
 // Functions
@@ -23,15 +40,58 @@ void initializePins() {
   pinMode(Config::PIN_ECHO, INPUT);
 }
 
-void setup() {
+
+void setup() 
+{
   Serial.begin(115200);
+
   initializePins();
 
   settingsManager.begin();
   settingsManager.load(settings);
   
+  sensorManager.begin();
+
+  Serial.println("System initialized");
 }
 
-void loop() {
+void loop() 
+{
+  const unsigned long now = millis();
 
+  // Sensor update
+  if (now - lastSensorUpdate >= SENSOR_INTERVAL_MS)
+  {
+    lastSensorUpdate = now;
+    sensorManager.update();
+  }
+
+
+  // Control intervall
+  if (now - lastControlUpdate >= CONTROL_INTERVAL_MS)
+  {
+    lastControlUpdate = now;
+
+    controlManager.update(
+      sensorManager.getData(),
+      settings
+    );
+  }
+
+  
+  // Log update
+   if (now - lastLogUpdate >= LOG_INTERVAL_MS)
+   {
+    lastLogUpdate = now;
+
+    const auto& data = sensorManager.getData();
+
+    Serial.printf(
+      "T=%.2f Cond=%.2f Turb=%.2f Level=%d\n",
+      data.temperature,
+      data.conductivity,
+      data.turbidity,
+      data.waterLevel
+    );
+   }
 }
