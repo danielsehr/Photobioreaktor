ec556ab3402829cf8822d6a953d8345078dbc4a8
danielsehr <daniel.serr7028@gmail.com>
2026-04-26 15:14:21 +0200

    add: SensorManager class, managing sensor reading methods

---
+++ include/sensors/SensorManager.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <core/SystemTypes.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+
+class SensorManager{
+    public:
+        SensorManager();
+        
+        void begin();
+        void update();  // reads all sensors once
+
+        const SensorData& getData() const;
+
+    private:
+        SensorData data;
+
+        // Hardware
+        OneWire oneWire;
+        DallasTemperature tempSensor;
+
+        float readTemperature();
+        float readTds(float temp);
+        float readTurbidity();
+        int readWaterLevel();
+}
