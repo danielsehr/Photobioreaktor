4a2e7b51463c6fb5f48dd26494e30fcb071f3cbf
danielsehr <daniel.serr7028@gmail.com>
2026-08-25 22:59:06 +0200

    moved SensorData struct to own script

---
+++ src/core/SensorData.h
@@ -0,0 +1,11 @@
+#pragma once 
+#include <Arduino.h>
+
+struct SensorData {
+    unsigned long uptimeSeconds = 0;
+
+    float temperature = NAN;
+    float conductivity = NAN;
+    float turbidity = NAN;
+    int waterLevel = -1;
+};
