fcd4475c5a13b06a71c9ae75ebd1f700dbc6befb
danielsehr <daniel.serr7028@gmail.com>
2026-08-26 11:55:56 +0200

    new: implemented dashboardService class from other project

---
+++ src/services/DashboardService/DashboardService.cpp
@@ -0,0 +1,74 @@
+#include "DashboardService.h"
+
+DashboardService::DashboardService(WebSocketManager& socket) 
+    : webSocketManager_(socket)
+{
+}
+
+void DashboardService::serializeMeasurement(const SensorData& data, char* buffer, std::size_t bufferSize) const
+{
+    JsonDocument json;
+
+    json["type"] = "sensor";
+
+    JsonObject values = json["data"].to<JsonObject>();
+
+    values["uptimeSeconds"] = data.uptimeSeconds;
+    values["temperature"] = data.temperature;
+    values["conductivity"] = data.conductivity;
+    values["turbidity"] = data.turbidity;
+    values["waterLevel"] = data.waterLevel;
+
+    serializeJson(json, buffer, bufferSize);
+}
+
+void DashboardService::publishMeasurement(const SensorData& data)
+{
+    history_.push(data);
+
+    char buffer[128];
+
+    serializeMeasurement(data, buffer, sizeof(buffer));
+
+    webSocketManager_.broadcast(buffer);
+}
+
+void DashboardService::publishHistory(AsyncWebSocketClient& client)
+{
+    char buffer[128];
+
+    for (size_t i = 0; i < history_.size(); i++)
+    {
+        const auto& measurement = history_.at(i);
+
+        serializeMeasurement(measurement, buffer, sizeof(buffer));
+        
+        webSocketManager_.send(client, buffer);
+    }
+}
+
+void DashboardService::sendHistoryBegin(AsyncWebSocketClient& client)
+{
+    JsonDocument json;
+
+    json["type"] = "history_begin";
+
+    char buffer[64];
+
+    serializeJson(json, buffer, sizeof(buffer));
+
+    webSocketManager_.send(client, buffer);
+}
+
+void DashboardService::sendHistoryEnd(AsyncWebSocketClient& client)
+{
+    JsonDocument json;
+
+    json["type"] = "history_end";
+
+    char buffer[64];
+
+    serializeJson(json, buffer, sizeof(buffer));
+
+    webSocketManager_.send(client, buffer);
+}
---
+++ src/services/DashboardService/DashboardService.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "config/SystemConfig.h"
+#include "core/SensorData.h"
+#include "web/WebSocketManager/WebSocketManager.h"
+#include "utils/RingBuffer/RingBuffer.h"
+
+class DashboardService
+{
+public: 
+    explicit DashboardService(WebSocketManager& socket);
+
+    void publishMeasurement(const SensorData& data);
+    
+    void publishHistory(AsyncWebSocketClient& client);
+    
+private:
+    WebSocketManager& webSocketManager_;
+    
+    RingBuffer<SensorData, Config::RING_BUFFER_SIZE> history_;
+    
+    void serializeMeasurement(const SensorData& data, char* buffer, std::size_t bufferSize) const;
+
+    void sendHistoryBegin(AsyncWebSocketClient& client);
+
+    void sendHistoryEnd(AsyncWebSocketClient& client);
+};
