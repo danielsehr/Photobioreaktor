d51e984b265979ce2c196d88ee126c711b8ed12b
danielsehr <daniel.serr7028@gmail.com>
2026-08-27 18:15:49 +0200

    add: experimentManager from other project

--- data/js/main.js
+++ data/js/main.js
@@ -1,10 +1,14 @@
 import { WebSocketClient } from "./web/websocket-client.js";
 import { CurrentMeasurements } from "./ui/current-measurements.js";
 import { ChartManager } from "./ui/chart-manager.js";
+import { ExperimentUi } from "./ui/experiment-ui.js";
+import { ExperimentManager } from "./experiment/experiment-manager.js";
 
 const socket = new WebSocketClient();
 const currentMeasurements = new CurrentMeasurements();
 const chartManager = new ChartManager();
+const experimentUi = new ExperimentUi();
+const experimentManager = new ExperimentManager(experimentUi);
 
 const connectionStatus = document.getElementById("connectionStatus")
 
@@ -26,3 +30,5 @@ socket.onMeasurement((measurement) => {
 
     chartManager.addMeasurement(measurement);
 });
+
+experimentManager.initialize()
