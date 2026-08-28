020f0649414d2176899266c228f59cf1739bbd22
danielsehr <daniel.serr7028@gmail.com>
2026-08-28 20:12:30 +0200

    add: SettingsManager, ExperimentManager, TimeApi, SettingsUi class, add: initialize method

--- data/js/main.js
+++ data/js/main.js
@@ -1,14 +1,28 @@
 import { WebSocketClient } from "./web/websocket-client.js";
 import { CurrentMeasurements } from "./ui/current-measurements.js";
 import { ChartManager } from "./ui/chart-manager.js";
+
+import { SettingsUi } from "./ui/settings-ui.js";
 import { ExperimentUi } from "./ui/experiment-ui.js";
-import { ExperimentManager } from "./experiment/experiment-manager.js";
+
+import { SettingsManager } from "./manager/settings-manager.js";
+import { ExperimentManager } from "./manager/experiment-manager.js";
+import { initialize } from "./manager/initialize-manager.js";
+
+import { TimeApi } from "./api/time-api.js";
+
 
 const socket = new WebSocketClient();
 const currentMeasurements = new CurrentMeasurements();
 const chartManager = new ChartManager();
+
 const experimentUi = new ExperimentUi();
+const settingsUi = new SettingsUi();
+
 const experimentManager = new ExperimentManager(experimentUi);
+const settingsManager = new SettingsManager(settingsUi);
+
+const timeApi = new TimeApi();
 
 const connectionStatus = document.getElementById("connectionStatus")
 
@@ -31,4 +45,5 @@ socket.onMeasurement((measurement) => {
     chartManager.addMeasurement(measurement);
 });
 
-experimentManager.initialize()
+
+initialize(settingsManager, experimentManager, settingsUi, timeApi);
