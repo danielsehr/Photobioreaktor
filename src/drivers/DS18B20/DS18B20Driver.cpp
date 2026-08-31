91066e7a580b12681659be2e7b0a26255bdceb00
danielsehr <daniel.serr7028@gmail.com>
2026-08-31 23:30:30 +0200

    add: count devices and logging

--- src/drivers/DS18B20/DS18B20Driver.cpp
+++ src/drivers/DS18B20/DS18B20Driver.cpp
@@ -1,4 +1,5 @@
 #include "DS18B20Driver.h"
+#include "utils/logger/Logger.h"
 
 DS18B20Driver::DS18B20Driver()
     :oneWire_(Config::PIN_ONEWIRE),
@@ -8,7 +9,18 @@ DS18B20Driver::DS18B20Driver()
 
 
 void DS18B20Driver::begin(){
+
     sensor_.begin();
+
+    if(sensor_.getDeviceCount() == 0)
+    {
+        LOG_INFO("[DS18B20 Driver] No temperature sensor found!");
+        return;
+    } 
+    else 
+    {
+        LOG_INFO("[DS18B20 Driver] %d sensor found!", sensor_.getDeviceCount());
+    }
 }
 
 
